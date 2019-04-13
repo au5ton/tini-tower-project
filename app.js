ed5c00933a05da9dc4f75a53fc03df60fd36b2e3
leo conforti <leo.conforti@tgeagle.org>
2019-04-13 14:11:30 -0500

    test from vscode

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 // {"player_id":"9GV2Y","player_ss":"efe5f6a3-8cd5-4956-897c-ec1db6c26485"}
 // {"player_id":"9GV59","player_ss":"be61b26e-330b-41e0-ad2f-48eb79dc3bd6"}
 
+// let a = new NimblebitClient("halfclownedwizard@gmail.com", "75XKD");
+// a.registerEmail();
+
 class NimblebitClient {
   constructor(cloud_email, player_id, player_ss) {
     this.authenticated = true;
@@ -19,7 +22,8 @@ class NimblebitClient {
     this.player_ss = player_ss | null;
     
     this.salt = 1;
-    this.host = "https://sync.nimblebit.com/";
+    this.host = "https://sync.nimblebit.com";
+    this.corsAnywhere = "https://cors-anywhere.herokuapp.com/";
     this.superSecret = "]m7PWGiQDLzQsTfh;^U&vumQ`;%t<Gt%dIa*u!THZbi_ya2J7t!*(9^G5<$rEDLo";
 
     this.burnBot = {
@@ -28,25 +32,27 @@ class NimblebitClient {
     };
   }
 
-  grabPlayerSalt() {
-
-  }
-
   registerEmail() {
     if (!this.authenticated) {
       let hash = md5("tt/" + this.burnBot.player_id + "/" + this.salt + this.cloud_email + this.burnBot.player_ss + this.superSecret);
-      
-      // axios({
-      //   method: "post",
-      //   url: this.host + "register_email/tt/" + this.burnBot.player_id + "/" + this.salt + "/" + hash + "?email=" + this.cloud_email + "&promote=1",
-      //   crossdomain: true,
-      //   data: {
-      //     email: this.cloud_email,
-      //     promote: 1
-      //   }
-      // }).then(function(response) {
-      //   console.log(response);
-      // });
+      let url = this.host + "/register_email/tt/" + this.burnBot.player_id + "/" + this.salt + "/" + hash + "?email=halfclownedwizard@gmail.com&promote=1";
+
+      let request = new XMLHttpRequest();
+      request.open('POST', this.corsAnywhere + url, true);
+
+      request.onload = function() {
+        if ((this.status == 200) && (this.readyState == 4)) {
+          let response = JSON.parse(this.response);
+          console.log(response);
+        } else {
+          // We reached our target server, but it returned an error
+          console.log(this.status);
+        }
+      };
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
+      request.send();
     } else {
       console.log("looks like your allready authenticated!");
     }
@@ -63,5 +69,3 @@ class NimblebitClient {
   }
 }
 
-let a = new NimblebitClient("halfclownedwizard@gmail.com", "75XKD");
-a.registerEmail();

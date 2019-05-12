e4d548906192bd4132b53c4e11cf50a8ea388be1
Leo Confroti <leo.conforti@tgeagle.org>
2019-05-11 23:51:51 -0500

    updates

--- NimblebitClient.js
+++ NimblebitClient.js
@@ -1,12 +1,5 @@
 /*jshint esversion: 8 */
 
-// {"player_id":"9GTYN","player_ss":"89f9b90b-4e1e-4b48-af56-df39da7b17a7"}
-// {"player_id":"9GV2Y","player_ss":"efe5f6a3-8cd5-4956-897c-ec1db6c26485"}
-// {"player_id":"9GV59","player_ss":"be61b26e-330b-41e0-ad2f-48eb79dc3bd6"}
-
-// let a = new NimblebitClient("halfclownedwizard@gmail.com", "75XKD");
-// a.registerEmail();
-
 class NimblebitClient {
   constructor(player_id, cloud_email, player_ss) {
     this.authenticated = true;
@@ -20,8 +13,9 @@ class NimblebitClient {
     this.player_id = player_id;
     this.cloud_email = cloud_email;
     this.player_ss = player_ss || null;
-    
+    this.currentVersion = 1;
     this.salt = 1;
+    
     this.host = "https://sync.nimblebit.com";
     this.corsAnywhere = "https://cors-anywhere.herokuapp.com/";
     this.superSecret = "]m7PWGiQDLzQsTfh;^U&vumQ`;%t<Gt%dIa*u!THZbi_ya2J7t!*(9^G5<$rEDLo";
@@ -32,79 +26,87 @@ class NimblebitClient {
     };
   }
 
-  checkFornewerSave() {
-    let hash = md5("tt/" + this.player_id + "/" + this.salt + this.player_ss + this.superSecret);
-    let url = this.host + "/sync/current_version/tt/" + this.player_id + "/" + this.salt + "/" + hash;
-    console.log(this.corsAnywhere + url);
-
-    let request = new XMLHttpRequest();
-    request.open('GET', this.corsAnywhere + url, true);
-
-    request.onload = function() {
-      if ((this.status == 200) && (this.readyState == 4)) {
-        let response = JSON.parse(this.response);
-        
-        let b = response.h;
-        let id = response.id;
-        if (md5(this.player_id + this.salt + id.toString() + this.player_ss + this.superSecret) == b) {
-          if (currentVersion != id) {
-            console.log("Sync version mismatch (local: " + currentVersion + ", server: " + id + "), getting server version.");
-            getCurrentSave();
-          } else {
-            console.log("Sync on current version.");
-          }
-        } else {
-          console.log("Sync error: Hash verification failed.");
-        }
-      } else {
-        // We reached our target server, but it returned an error
-        console.log("sync api error or network error. http status code: " + this.status);
-      }
-    };
-    request.onerror = function() {
-      // There was a connection error of some sort
-    };
-    request.send();  
+  static regenSalt() {
+    return new Promise(function(resolve, reject) {
+      //generate random crypto bits here
+    });
   }
 
-  getCurrentSave() {
+  static computeMd5(input) {
+    if (typeof input != String) {
+      input = input.toString();
+    }
+    return new Promise(function(resolve, reject) {
+      resolve(md5(input));
+    });
+  }
+
+  static serverRequest(method, url) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      request.open(method, url, true);
+
+      request.onload = function() {
+        if ((this.status == 200) && (this.readyState == 4)) {
+          resolve(request.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: request.statusText,
+          });
+        }
+      }
+      request.onerror = function() {
+        reject({
+          status: this.status,
+          statusText: this.statusText,
+        });
+      }
+      request.send();
+    });
+  }
+
+  async checkFornewerSave() {
+    let hash = md5("tt/" + this.player_id + "/" + this.salt + this.player_ss + this.superSecret);
+    let url = this.host + "/sync/current_version/tt/" + this.player_id + "/" + this.salt + "/" + hash;
+
+    let incoming = await NimblebitClient.serverRequest('GET', this.corsAnywhere + url);
+    let response = JSON.parse(incoming);
+    let h = response.h;
+    let id = response.id;
+
+    if (await NimblebitClient.computeMd5(this.player_id + this.salt + id.toString() + this.player_ss + this.superSecret) == h) {
+      if (this.currentVersion != id) {
+        console.log("Sync version mismatch (local: " + this.currentVersion + ", server: " + id + "), getting server version.");
+        this.getCurrentSave();
+      } else {
+        console.log("Sync on current version.");
+      }
+    } else {
+      console.log("Sync error: Hash verification failed.");
+    }
+  }
+
+  async getCurrentSave() {
     let hash = md5("tt/" + this.player_id + "/" + this.salt + this.player_ss + this.superSecret);
     let url = this.host + "/sync/pull/tt/" + this.player_id + "/" + this.salt + "/" + hash;
 
-    let request = new XMLHttpRequest();
-    request.open('GET', this.corsAnywhere + url, true);
-    let _this = this;
+    let incoming = await NimblebitClient.serverRequest('GET', this.corsAnywhere + url);
+    console.log("downloading new sync data...");
+    let response = JSON.parse(incoming);
+    let arg = response.id;
+    let h = response.h;
+    let data = response.data;
 
-    request.onload = function() {
-      if ((this.status == 200) && (this.readyState == 4)) {
-        let response = JSON.parse(this.response);
-        console.log(response);
-
-        let arg = response.id;
-        let b = response.h;
-        let data = response.data;
-
-        if (md5(_this.player_id + _this.salt + arg.toString() + data + _this.player_ss + _this.superSecret) == b) {
-
-        } else {
-          console.log("Sync pull error: hash verificatiopn failed");
-        }
-      } else {
-        // We reached our target server, but it returned an error
-        console.log("Sync api or network error. http status code: " + this.status);
-      }
-    };
-    request.onerror = function() {
-      // There was a connection error of some sort
-    };
-    request.send();
+    if (await NimblebitClient.computeMd5(this.player_id + this.salt + arg.toString() + data + this.player_ss + this.superSecret) == h) {
+      console.log("downloaded sync hash verification passed!");
+      console.log(response);
+    } else {
+      console.log("Sync pull error: hash verificatiopn failed");
+    }
   }
 
-  decompress(gzip) {
-    
-  }
-
-  registerEmail() {
+  async registerEmail() {
     if (!this.authenticated) {
       let hash = md5("tt/" + this.burnBot.player_id + "/" + this.salt + this.cloud_email + this.burnBot.player_ss + this.superSecret);
       let url = this.host + "/register_email/tt/" + this.burnBot.player_id + "/" + this.salt + "/" + hash + "?email=halfclownedwizard@gmail.com&promote=1";
@@ -134,19 +136,11 @@ class NimblebitClient {
     
   }
 
-  async regenSalt() {
-    return new Promise(function(resolve, reject) {
-
-    });
-  }
-
   newPlayer() {
     let random1 = parseInt((Math.random() + ' ').substring(2, 11));
     let random2 = parseInt((Math.random() + ' ').substring(2, 11));
     let hash = md5("tt/" + random1.toString() + "/" + random2.toString() + this.superSecret);
     let url = this.host + "/register/tt/" + random1.toString() + "/" + random2.toString() + "/" + hash;
-
-
   }
 }
 
